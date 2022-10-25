35e571462600fb76fbfd0c22c930bfa7c019a97c
addsch <addison148@gmail.com>
2022-10-25 15:03:32 -0700

    fix commands

--- src/Events/messageCreate.js
+++ src/Events/messageCreate.js
@@ -9,9 +9,9 @@ module.exports = new Event("messageCreate", (config, message) => {
     const args = message.content.substring(config.prefix.length).split(/ +/);
 
     //print error message if command doesn't exist
-    const command = config.commands.find(cmd => {
-        cmd.name === args[0];
-    });
+    const command = config.commands.find(cmd =>
+        cmd.name === args[0]
+    );
     if (!command) return message.reply(`:warning: *Command* \`!${args[0]}\` *does not exist.* :warning:`);
 
     command.run(message, args, config);
