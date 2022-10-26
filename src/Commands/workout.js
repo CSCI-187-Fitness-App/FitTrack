b57d6c7b85b1b6e5ac4f90ddea149a0437d01a7b
Cristobal de Oyarzabal <cristobaldeoyarzabal@Hades.local>
2022-10-25 18:03:45 -0700

    commiting workout command

--- src/Commands/workout.js
+++ src/Commands/workout.js
@@ -1,18 +1,27 @@
 const Command = require("../Structures/Command.js");
+const {EmbedBuilder} = require("discord.js");
 
 module.exports = new Command({
     name: "workout",
     description: "Checks workout of the day and returns exercises, sets/reps, and wieghts.",
 
     async run(message, args, client) {
+        const workoutEmbdm = new EmbedBuilder()
+        .setTitle('Workout of the day: Leg Day')
+        .addFields(
+            {name: 'Workout of the day:', value: 'Leg Day'},
+            {name: 'Excersise:', value: 'squats'},
+            {name: 'Sets/reps:', value: '3/6', inline: true},
+            {name: 'Weight:', value: '135 155 155', inline: true},
+            {name: 'Excersise:', value: 'calf raises'},
+            {name: 'Sets/reps:', value: '3/6', inline: true},
+            {name: 'Weight:', value: '135 155 155', inline: true},
+            {name: 'Excersise:', value: 'RDL'},
+            {name: 'Sets/reps:', value: '3/8', inline: true},
+            {name: 'Weight:', value: '155 175 175', inline: true},
 
-        message.reply(`Workout of the day: Leg Day \n
-        Excersise: squats \n
-        Sets/reps: 3/6 Weight: 135 155 155 \n
-        Excersise: calf raises\n
-        Sets/reps: 3/6 Weight: 135 155 155\n
-        Excersise: RDL\n
-        Sets/reps: 3/6 Weight: 155 175 175`); 
+        );
+        message.reply({embeds: [workoutEmbdm]});
 
 
     }
