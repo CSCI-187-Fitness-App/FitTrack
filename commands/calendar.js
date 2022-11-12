93ecf532c800aae0d8f5e3a4b657fea8ecf70306
addsch <addison148@gmail.com>
2022-11-12 14:52:40 -0800

    restructure everything to move from discord.js v13 to v14

---
+++ commands/calendar.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('calendar')
+		.setDescription('Display image of a week calendar of your workouts.'),
+	async execute(interaction) {
+        let topline =    "-------------------------------\n";
+        let bottomline = "\n-------------------------------";
+        const calendarEmbed = new EmbedBuilder()
+            .setColor(0x800020)
+            .setTitle("Weekly View of workouts:")
+            .setDescription("\u200b")
+            .addFields(
+                {name: topline + "Monday" + bottomline, value:"- Flat Bench Press \n - Inclined Bench Press \n - Dumbell Fly \n - Tricep push downs \n - Bicep curls", inline: true},
+                {name: topline + "Tuesday" + bottomline, value: "Rest Day", inline: true},
+                {name: topline + "Wednesday" + bottomline, value: "Vinyasa Yoga", inline: true},
+                {name: topline + "Thursday" + bottomline, value: "Rest Day", inline: true},
+                {name: topline + "Friday" + bottomline, value: "- Deadlift \n - RDLs \n - Sitting rows \n - Lat pulldowns \n - Shoulder press", inline: true},
+                {name: topline + "Saturday" + bottomline, value: "Rest Day", inline: true},
+                {name: topline + "Sunday" + bottomline, value: "Rest Day", inline: true},
+            );
+        interaction.reply({
+            embeds: [calendarEmbed]
+        });
+	},
+};
