e664cf097b79f75eef08e1db0ca4dfb922a9267b
Cristobal de Oyarzabal <cristobaldeoyarzabal@Hades.local>
2022-11-25 20:13:33 -0600

    set routine command

---
+++ commands/setroutine.js
@@ -0,0 +1,65 @@
+// "!setRoutine" command
+//set routine is supposed to give the user a modal to enter their routine
+//when entering their routine they should select a day then in a text box
+//enter what their workout is for that day
+const { 
+    
+    ActionRowBuilder, 
+    //Events, 
+    //TextInputBuilder, 
+    //TextInputStyle,
+    SlashCommandBuilder,
+    SelectMenuBuilder
+} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+        .setName('setroutine')
+		.setDescription('Set the routine for a specific day'),
+	async execute(interaction) {
+        const days = new ActionRowBuilder()
+            .addComponents(
+                new SelectMenuBuilder()
+                    .setCustomId('routineSelectDay')
+                    .setPlaceholder('Choose day to place routine in')
+                    .setMinValues(1)
+                    .setMaxValues(7)
+                    .addOptions( 
+                        {
+                            label: 'Sunday',
+                            value: 'Sunday',
+                        },
+                        {
+                            label: 'Monday',
+                            value: 'Monday',
+                        },
+                        {
+                            label: 'Tuesday',
+                            value: 'Tuesday',
+                        },
+                        {
+                            label: 'Wednesday',
+                            value: 'Wednesday',
+                        },
+                        {
+                            label: 'Thursday',
+                            value: 'Thursday',
+                        },
+                        {
+                            label: 'Friday',
+                            value: 'Friday',
+                        },
+                        {
+                            label: 'Saturday',
+                            value: 'Saturday',
+                        }
+                    )
+            )
+            
+            await interaction.reply({
+                content: "Select a day to place routine in.",
+                components: [type]
+            });
+
+	}
+};
