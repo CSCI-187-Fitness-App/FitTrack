45d030bcd9e2dd3ba0571ba06cfdd8550591ca07
bryanvtam <42446827+bryanvtam@users.noreply.github.com>
2022-11-27 18:38:34 -0800

    addworkout command

---
+++ commands/addworkout.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder, ActionRowBuilder, Events, ModalBuilder, TextInputBuilder, TextInputStyle, CommandInteractionOptionResolver} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('addworkout')
+        .setDescription('add a workout to routines database'),
+    async execute(interaction){
+        if(interaction.setCustomId == "exerciseModal"){
+            await CommandInteractionOptionResolver.reply({content: "Submitted"});
+        }
+
+        const modal = new ModalBuilder()
+			.setCustomId('excerciseModal')
+			.setTitle('My exercise');
+
+        const exerciseName = new TextInputBuilder()
+			.setCustomId('exerciseName')
+		    // The label is the prompt the user sees for this input
+			.setLabel("Type the name of the exercise")
+		    // Short means only a single line of text
+			.setStyle(TextInputStyle.Short);
+
+        const exerciseActionRow = new ActionRowBuilder().addComponents(exerciseName);
+        modal.addComponents(exerciseActionRow);
+
+        await interaction.showModal(modal);
+    },
+};
